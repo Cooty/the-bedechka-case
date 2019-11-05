83ce6e5ff8a2f2b7a27e8463c6306894481d7ae4
Cooty <cooty13@gmail.com>
2019-11-05 09:58:17 +0100

    [WIP] Timeline module
    - Add elements to own function

--- assets/ts/modules/timeline.ts
+++ assets/ts/modules/timeline.ts
@@ -1,9 +1,9 @@
 export class Timeline {
 
     private readonly timeline: Element;
-    private readonly backButton: Element;
-    private readonly forwardButton: Element;
-    private readonly scrollPlane: Element;
+    private backButton: Element;
+    private forwardButton: Element;
+    private scrollPlane: Element;
 
     constructor(timeline: Element) {
         this.timeline = timeline;
@@ -12,14 +12,18 @@ export class Timeline {
             return;
         }
 
-        this.backButton = this.timeline.querySelector('.js-timeline-back');
-        this.forwardButton = this.timeline.querySelector('.js-timeline-forward');
-        this.scrollPlane = this.timeline.querySelector('.js-timeline-steps');
+        this.getElements();
 
         if(this.backButton && this.forwardButton && this.scrollPlane) {
             this.init();
         }
 
+    }
+
+    private getElements() {
+        this.backButton = this.timeline.querySelector('.js-timeline-back');
+        this.forwardButton = this.timeline.querySelector('.js-timeline-forward');
+        this.scrollPlane = this.timeline.querySelector('.js-timeline-steps');
     }
 
     public init() {
