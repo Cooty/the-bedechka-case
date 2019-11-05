d420c4628c483403be76731a8096d8be4c971df5
Cooty <cooty13@gmail.com>
2019-11-05 10:13:34 +0100

    [WIP] Timeline module
    - Add scafold for the module with placeholder methods

--- assets/ts/modules/timeline.ts
+++ assets/ts/modules/timeline.ts
@@ -26,6 +26,30 @@ export class Timeline {
         this.scrollPlane = this.timeline.querySelector('.js-timeline-steps');
     }
 
+    private getStepWidth() {
+        // get the number of pixels which to move the timeline
+    }
+
+    private moveForward() {
+        // move the scroll plane by one slide forward
+    }
+
+    private moveBackward() {
+        // move the scroll plane by one slide backward
+    }
+
+    private hideBackButton() {
+        // hide the back button when reaching the start of the timeline
+    }
+
+    private hideForwardButton() {
+        // hide the forward button when reaching the end of the timeline
+    }
+
+    private addEventHandlers() {
+        // hook up all functions to events
+    }
+
     public init() {
         console.log('Timeline module reporting for duty');
     }
