f16bea73a76e962d2a0158e6ecc8bf580934e044
Cooty <cooty13@gmail.com>
2021-12-30 18:18:08 +0100

    Change nev file name

--- app/scripts/create_env_file.sh
+++ app/scripts/create_env_file.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 env_vars=(APP_ENV APP_SECRET HOST_NAME DATABASE_NAME DATABASE_URL MAILER_URL)
-file_name=".env.dev.local"
-if [ "$1" == "prod" ]; then
-    file_name=".env"
-fi
+file_name=".env.local"
 file_content=""
 
 for e in "${env_vars[@]}"
